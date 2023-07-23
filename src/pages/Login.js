8264a4676ee5d6bc4d6e0d754afed2037ee9b13b
user1 <user1@abc.com>
2023-07-23 14:31:19 -0400

    add community and footer to login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Header from '../components/Header'
 import Navigation from '../components/Navigation'
+import CommunityInfo from '../components/CommunityInfo'
+import Footer from '../components/Footer'
 
 const Login = () => {
   return (
     <section className='loginPage'>
       <Header />
       <Navigation active='login'/>
+      <h1>Login details here...</h1>
+      <CommunityInfo />
+      <Footer />
     </section>
   )
 }
