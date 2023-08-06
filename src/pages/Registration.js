85206e8bb8c3de382dd1bab2a1f47ac3a4bda6fc
user1 <user1@abc.com>
2023-08-05 21:00:00 -0400

    fix registration issue

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -7,43 +7,52 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
 const Registration = () => {
-    const [name,setName] = useState('')
-    const [dob,setDob] = useState('')
-    const [gender,setGender] = useState('');
-    const [hobbies,setHobbies]=useState('');
-    const [email,setEmail]=useState('')
-    const [password,setPassword]=useState('')
-    const [confPassword,setConfPassword]=useState('')
-    const [success,setSuccess] = useState(false);
+    // const [name,setName] = useState('')
+    // const [dob,setDob] = useState('')
+    // const [gender,setGender] = useState('');
+    // const [hobbies,setHobbies]=useState('');
+    // const [email,setEmail]=useState('')
+    // const [password,setPassword]=useState('')
+    // const [confPassword,setConfPassword]=useState('')
+    // const [success,setSuccess] = useState(false);
 
+    const [registration,setRegistration] = useState({name:'',dob:'',gender:'',hobbies:'',email:'',password:'',confPassword:''});
+
+    const updateState = (e)=>{
+        console.log(e.target.name,e.target.value)
+        setRegistration({...registration,[e.target.name]:e.target.value})
+    }
     const navigate = useNavigate();
 
     function selectHobbies(e){
         const str = e.target.previousSibling.textContent.trim();
-        const startIndex = hobbies.indexOf(str);
+        console.log(registration)
+        const startIndex = registration.hobbies.indexOf(str);
         const stringLength = e.target.previousSibling.length;
         console.log('String found at index: ',startIndex)
         if(startIndex>=0){
             //console.log('Slice: '+hobbies.splice(startIndex,stringLength+1))
-            const newString = hobbies.slice(0,startIndex)+hobbies.substring((startIndex+stringLength))
+            const newString = registration.hobbies.slice(0,startIndex)+registration.hobbies.substring((startIndex+stringLength))
             //console.log(newString)
-            setHobbies(newString)
+            setRegistration({...registration,hobbies:newString})
         } else {    
-            if(hobbies.length==0){
-                setHobbies(str); //coding
+            if(registration.hobbies.length==0){
+                //setHobbies(str); //coding
+                setRegistration({...registration,hobbies:str})
             }  else {
-                setHobbies(hobbies+","+str); //'coding',"reading"
+                //setHobbies(); //'coding',"reading"
+                setRegistration({...registration,hobbies:registration.hobbies+","+str})
             }
         }
         
     }
     const register = async()=>{
-        if(password===confPassword){
+        if(registration.password===registration.confPassword){
             const response = await axios.post('http://localhost:8000/api/v1/users',{
-                name,dob,gender,hobbies,email,password
+                ...registration
             }) 
             if(response.status===201){
-                setSuccess(true);
+                setRegistration({...registration,success:true});
                 setTimeout(()=>{
                     navigate('/login')
                 },2000)                
@@ -62,57 +71,60 @@ const Registration = () => {
       <Navigation active='register'/>
       <article className="register_hero">
         <form className="register_hero_form" onSubmit={registerUser}>
-            {success && <h3 className='success'>Registeration is successful!</h3>}
+            {registration.success && <h3 className='success'>Registeration is successful!</h3>}
             <div className="register_hero_form_detail">
                 <label htmlFor="name">Name:</label>
-                <input type="text" id="name"
-                value={name}
-                onChange={(event)=>setName(event.target.value)}
+                <input type="text" id="name" name='name'
+                value={registration.name}
+                onChange={(event)=>updateState(event)}
                 placeholder='Enter your name..'/>
             </div>
             <div className="register_hero_form_detail">
                 <label htmlFor="dob">Brith Date:</label>
-                <input type="date" id="dob"
-                value={dob}
-                onChange={(e)=>setDob(e.target.value)}
+                <input type="date" id="dob" name='dob'
+                value={registration.dob}
+                onChange={(event)=>updateState(event)}
                 placeholder='mm/dd/yyyy'/>
             </div>
             <div className="register_hero_form_detail gender">
                 <label>Gender:</label>
-                Male <input type='radio' name='gender' value={gender} onChange={(e)=>setGender('Male')}/>
-                Female <input type='radio' name='gender' value={gender} onChange={(e)=>setGender('Female')} />
-                Other <input type='radio' name='gender' value={gender} onChange={(e)=>setGender('Other')}/>
+                Male <input type='radio' name='gender'
+                onChange={()=>setRegistration({...registration,gender:'Male'})}/>
+                Female <input type='radio' name='gender'
+                onChange={()=>setRegistration({...registration,gender:'Female'})}/>
+                Other <input type='radio' name='gender'
+                onChange={()=>setRegistration({...registration,gender:'Other'})}/>
             </div>
             <div className="register_hero_form_detail hobbies">
                 <label>Hobbies:</label>
-                 Coding <input type='checkbox' value={hobbies.indexOf('coding')>=0?true:false}
+                 Coding <input type='checkbox'
                  onChange={selectHobbies}/>
-                 Movies <input type='checkbox' value={hobbies.indexOf('movies')>=0?true:false}
+                 Movies <input type='checkbox'
                  onClick={selectHobbies}/>
-                 Exercise <input type='checkbox' value={hobbies.indexOf('excercise')>=0?true:false}
+                 Exercise <input type='checkbox'
                  onClick={selectHobbies}/>
-                 Reading <input type='checkbox' value={hobbies.indexOf('reading')>=0?true:false}
+                 Reading <input type='checkbox'
                  onClick={selectHobbies}/>
             </div>
             <div className="register_hero_form_detail">
                 <label htmlFor="email">Email:</label>
-                <input type="email" id="email"
-                value={email}
-                onChange={(e)=>setEmail(e.target.value)}
+                <input type="email" id="email" name='email'
+                value={registration.email}
+                onChange={(event)=>updateState(event)}
                 placeholder='Enter your email..'/>
             </div>
             <div className="register_hero_form_detail">
                 <label htmlFor="pwd">Password:</label>
-                <input type="password" id="pwd"
-                value={password}
-                onChange={(e)=>setPassword(e.target.value)}
+                <input type="password" id="pwd" name='password'
+                value={registration.password}
+                onChange={(event)=>updateState(event)}
                 placeholder='Enter your password..'/>
             </div>
             <div className="register_hero_form_detail">
                 <label htmlFor="cpwd">Confirm Password:</label>
-                <input type="password" id="cpwd"
-                value={confPassword}
-                onChange={(e)=>setConfPassword(e.target.value)}
+                <input type="password" id="cpwd" name='confPassword'
+                value={registration.confPassword}
+                onChange={(event)=>updateState(event)}
                 placeholder='Enter same password..'/>
             </div>
             <div className="register_hero_cta">
